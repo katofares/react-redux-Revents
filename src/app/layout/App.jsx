752ac3aebe9841b3566b2b74283fed271b21e043
Fares Juma Kato <pshihashafares@gmail.com>
2018-05-27 07:22:52 +0300

    static data implemented

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react'
+// import { Button } from 'semantic-ui-react'
+import  EventDashboard  from '../../features/event/eventDashboard/EventDashboard'
+import NavBar from '../../features/nav/navBar/navBar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> Re-vent </h1>
+      <div>
+        <NavBar/> 
+        <Container className="main">       
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
